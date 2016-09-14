43f9a3e93948da99b2d0ad78316611997d38e32c
ninofabrizio <nin0_w3@hotmail.com>
2016-09-14 17:12:56 -0300

    String and Char treated

--- main.c
+++ main.c
@@ -6,7 +6,11 @@ int main( int argc, char *argv[]) {
     
 	int token;
 	FILE *file;
-	
+
+	// just tests...
+	//char _ = '.';
+	//printf("\a");
+
     if (argc > 0) {
 
         file = fopen(argv[0], "r");
@@ -16,13 +20,22 @@ int main( int argc, char *argv[]) {
     else
         file = stdin;
     
-	while ( token = yylex()  ) {
-		
-		if(token == 37)
-			printf("\nString: %d\n", yyvar.i);
-		else
+	while( token = yylex() ) {
+
+		if(token != -1) {
 			printf("\ntoken: %d\n", token);
-			
+
+			if(token == 34)
+				printf("\nChar: %c\n", yyvar.i);
+			else if(token == 35)
+				printf("\nString: %s$ of Size: %d\n", yyvar.s, strlen(yyvar.s));
+			else if(token == 37)
+				printf("\nHexa: %x\n", yyvar.i);
+			else
+				printf("\ntoken: %d\n", token);
+		}
+
+		else
+			break;
 	}
-	
 }
