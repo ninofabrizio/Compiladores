3ab6a2a515d93312b41f6c56a4fd3b8847a5e183
Leandro Morgado <leandro-morg@hotmail.com>
2016-09-30 15:28:54 -0300

    final

--- automatic.sh
+++ automatic.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-echo "\n\nINICIANDO TESTES - ANALIZADOR LEXICO\n"
+echo "\n\nINICIANDO TESTES - ANALIZADOR SINTATICO\n"
 
 for file in syntax_examples/*.in
 do
     filename=$(basename "$file")
     filename="${filename%.*}"
 
-    echo "\n\n++++++TESTANDO $filename\n"
+    echo "\n++++++TESTANDO $filename\n"
 	
     if ( ./tester < syntax_examples/$filename.in ) then
-    	echo "ARQUIVO PASSOU\n"	
+    	echo "      ARQUIVO PASSOU\n"	
 	else
-		echo "ARQUIVO NAO PASSOU\n"
+		echo "      ARQUIVO NAO PASSOU\n"
 	fi
 
     echo "------TERMINADO $filename\n"
 done
 
-echo  "\n\nTESTES ACABADOS - ANALIZADOR LEXICO\n"
+echo  "\nTESTES ACABADOS - ANALIZADOR SINTATICO\n"
